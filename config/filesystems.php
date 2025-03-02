a7b5281c873ed5400238acd0f2e8294d049fb2b8
jrdemadara <jrdemadara@protonmail.com>
2025-03-02 22:16:48 +0800

    fix: filesystem default storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DISK', 'local'),
+    'default' => env('FILESYSTEM_DISK', 'external_storage'),
 
     /*
     |--------------------------------------------------------------------------
@@ -28,42 +28,41 @@ return [
     |
     */
 
-    'disks' => [
+    'disks'   => [
 
-        'local' => [
+        'local'            => [
             'driver' => 'local',
-            'root' => storage_path('app/private'),
-            'serve' => true,
-            'throw' => false,
+            'root'   => storage_path('app/private'),
+            'serve'  => true,
+            'throw'  => false,
             'report' => false,
         ],
 
-        'public' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL') . '/storage',
+        'public'           => [
+            'driver'     => 'local',
+            'root'       => storage_path('app/public'),
+            'url'        => env('APP_URL') . '/storage',
             'visibility' => 'public',
-            'throw' => false,
-            'report' => false,
+            'throw'      => false,
+            'report'     => false,
         ],
 
         'external_storage' => [
             'driver' => 'local',
-            'root' => storage_path('../../azitsorog-backend/storage/app'),
+            'root'   => storage_path('../../azitsorog-backend/storage/app'),
         ],
 
-
-        's3' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
+        's3'               => [
+            'driver'                  => 's3',
+            'key'                     => env('AWS_ACCESS_KEY_ID'),
+            'secret'                  => env('AWS_SECRET_ACCESS_KEY'),
+            'region'                  => env('AWS_DEFAULT_REGION'),
+            'bucket'                  => env('AWS_BUCKET'),
+            'url'                     => env('AWS_URL'),
+            'endpoint'                => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw' => false,
-            'report' => false,
+            'throw'                   => false,
+            'report'                  => false,
         ],
 
     ],
@@ -79,7 +78,7 @@ return [
     |
     */
 
-    'links' => [
+    'links'   => [
         public_path('storage') => storage_path('app/public'),
     ],
 
