6d27f0cac2a1592158ef3b34c84730049e08ca5e
jrdemadara <jrdemadara@protonmail.com>
2025-07-30 14:35:02 +0800

    fix wen.php

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ use App\Http\Controllers\SecureFileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", function () {
-    abort(403);
+    return view("welcome");
 });
 
 Route::middleware(["auth"])->group(function () {
     Route::get("/secure-file/{type}/{filename}", [SecureFileController::class, "show"])->name(
-        "secure.file"
+        "secure.file",
     );
 });
